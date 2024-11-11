a6a30782543901f0e877ce9a4f7c79c3ba63898d
isuru117 <isuruedirisinghe80@gmail.com>
2024-11-11 10:47:40 +0100

    allow localhost CORS origin

--- archimed_case/settings.py
+++ archimed_case/settings.py
@@ -42,7 +42,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    os.getenv('CORS_ALLOWED_ORIGINS')
+    os.getenv('CORS_ALLOWED_ORIGINS'),
+    'http://127.0.0.1:5173'
 ]
 
 ROOT_URLCONF = 'archimed_case.urls'
